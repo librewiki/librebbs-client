c843f09ad41ee1bca1a98a0e0cc2b79464318320
Nessun Kim <nessunkim@gmail.com>
2021-07-24 16:43:51 +0900

    Remove unused import

--- src/views/TopicList.vue
+++ src/views/TopicList.vue
@@ -12,7 +12,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 import InfiniteLoading, { StateChanger } from "vue-infinite-loading";
 import { getTopics } from "@/api";
 import type { Topic, Board } from "@/api";
