c1de9178a51eef7db46b7040a11ed7e64aa3d25a
zlzleking <zlzleking@gmail.com>
2021-07-27 14:44:05 +0900

    fix workflows

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 apk update
 apk upgrade
 npm install
-${env | grep VUE_APP_CLIENT_ID} > .env.local
-${env | grep VUE_APP_BACKEND_HOST} >> .env.local
+$(env | grep VUE_APP_CLIENT_ID} ) .env.local
+$(env | grep VUE_APP_BACKEND_HOST) >> .env.local
